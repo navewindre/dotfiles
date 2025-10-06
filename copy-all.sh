6faa08dd69c775670f58a488c64aaa274132708a
xo <boneyaard@gmail.com>
2025-10-06 23:27:09 +0200

    update theme

--- copy-all.sh
+++ copy-all.sh
@@ -23,7 +23,7 @@ cp -f ./zshrc ~/.zshrc
 rm ~/.config/nvim/init.vim
 ln -s ~/.nvimrc ~/.config/nvim/init.vim
 
-gsettings set org.gnome.desktop.interface gtk-theme 'platinum-custom'
+gsettings set org.gnome.desktop.interface gtk-theme 'ayaya'
 gsettings set org.gnome.desktop.interface icon-theme 'NineIcons'
 
 pushd $PWD
